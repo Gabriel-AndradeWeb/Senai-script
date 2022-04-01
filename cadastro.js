5a1014f59393409575f88b4ccee8968f74860501
Gabriel-AndradeWeb <gabrielloliveira741@gmail.com>
2022-04-01 15:21:53 -0300

    ativiade cadastro

---
+++ cadastro.js
@@ -0,0 +1,27 @@
+var quantidadepessoas 
+var datadovento 
+var idade
+
+const entrada = require('prompt-sync')({sigint: true});
+
+datadovento = entrada("digite data do evento:");
+
+idade = entrada("digite sua idade:");
+
+quantidadepessoas = entrada("digite quantidade de pessoas:");
+
+    
+if( datadovento <= 20 ){
+    console.log("data do evento agendada !!")
+} else if ( datadovento >= 20) {
+    console.log("data inválida")
+}if( idade >= 18){
+    console.log("idade valida")
+}else if( idade < 18){
+    console.log("idade invalida")
+}if ( quantidadepessoas <= 100){
+    console.log("cadastro do evendo finalizado")
+}else{
+    console.log("não foi possivel fazer o cadastro")
+}
+
